9e29085f371e2d9a88115cad6d0899cc7e06e9da
subaru-hello <octosubaru0926@gmail.com>
2026-03-09 21:48:29 +0900

    chore: clean lint warnings

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,7 +1,6 @@
 // app/(tabs)/_layout.tsx
 import { Tabs } from "expo-router";
 import { Ionicons } from "@expo/vector-icons";
-import { Stack } from "expo-router";
 
 export default function TabLayout() {
   return (
